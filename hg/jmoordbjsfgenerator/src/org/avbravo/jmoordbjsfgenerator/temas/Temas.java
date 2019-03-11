7074891932a7afaf3b93b33a732c0e1f43162f25
avbravo <avbravo@avbravo>
2019-03-11 12:21:48 -0500

    0.1

---
+++ hg/jmoordbjsfgenerator/src/org/avbravo/jmoordbjsfgenerator/temas/Temas.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.avbravo.jmoordbjsfgenerator.temas;
+
+/**
+ *
+ * @author avbravo
+ */
+public class Temas {
+
+    String tema;
+
+    public Temas() {
+    }
+
+    public Temas(String tema) {
+        this.tema = tema;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    
+    
+}
